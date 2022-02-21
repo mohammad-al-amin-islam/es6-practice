d821a2f529c8601fb9a2b903da9d9380c9c8dced
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-02-21 17:52:38 +0600

    given es6 practice solve added

---
+++ es6-practice.js
@@ -0,0 +1,105 @@
+/* //১. কখন const আর কখন let দিয়ে ভেরিয়েবল ডিক্লেয়ার করতে হয় সেটা তোমাকে জানতেই হবে। তুমি নিজে নিজে একটা const দিয়ে আরেকটা let দিয়ে ভেরিয়েবল ডিক্লেয়ার করে ফেলো।
+
+const name = 'alamin';
+let age = 24;
+
+//২. টেম্পলেট স্ট্রিং দিয়ে একটা স্ট্রিং তৈরি করো। সেটার মধ্যে উপরে ডিক্লেয়ার করা ভেরিয়েবল এর মান ডাইনামিক ভাবে বসাও। একইভাবে উপরে একটা অবজেক্ট ডিক্লেয়ার করো। এবং ডাইনামিকভাবে উপরের অবজেক্ট এর দুইটা প্রপার্টি এর মান তোমার টেমপ্লেট স্ট্রিং এর মধ্যে বসাও। 
+const myObject = {
+    name:'alamin',
+    age :24,
+    height:6.1
+}
+const templateString = `My name is ${name}.My age is ${myObject.age} and My height is ${myObject.height}`
+console.log(templateString); */
+
+/* //৩.১ একটা প্যারামিটার ওয়ালা arrow ফাংশন ডিক্লেয়ার করো। এবং সেই ফাংশনের কাজ হবে তুমি কোন ইনপুট দিলে সেই ইনপুট সংখ্যাকে ৫ দিয়ে ভাগ করে আউটপুট দিবে। 
+
+const divBy5 = x => x/5;
+console.log(divBy5(10)); */
+
+/* //৩.২ তুমি দুইটা প্যারামিটার ওয়ালা একটা অ্যারো ফাংশন লিখবে। সেই ফাংশনের ভিতরে কাজ হবে। প্রত্যেকটা ইনপুট প্যারামিটার এর সাথে ২ যোগ করবে তারপর যোগফল দুইটা গুণ করবে। ক্যামনে করবে সেটা চিন্তা করে বের করার চেষ্টা করো 
+
+const multiply = (x, y) => (x + 2) * (y + 2);
+console.log(multiply(2,3)); */
+
+/* //৩.৩ এইবার তিনটা প্যারামিটার ওয়ালা একটা অ্যারো ফাংশন ডিক্লেয়ার করো। যেই ফাংশনের কাজ হবে তিনটা প্যারামিটার নিয়ে সেই তিনটা প্যারামিটারকে গুণ করে সেই রেজাল্ট রিটার্ন করবে। 
+
+const multiply = (x, y, z) => (x * y * z);
+console.log(multiply(1,2,3)); */
+
+/* //৩.৪ এইবার দুইটা প্যারামিটার ওয়ালা একটা অ্যারো ফাংশন নিবে। ওই arrow ফাংশনটা হবে অনেকগুলা লাইনের। সেখানে প্রত্যেকটা ইনপুট প্যারামিটার এর সাথে ২ যোগ করবে তারপর যোগফল দুইটা গুণ করবে। ক্যামনে করবে সেটা চিন্তা করে বের করার চেষ্টা করো। 
+
+const doMath = (x, y) => {
+    const a = x + 2;
+    const b = y + 2;
+    const result = a * b;
+    return result;
+}
+console.log(doMath(3, 3)); */
+
+/* //৫. অনেকগুলা সংখ্যার একটা array হবে। তারপর তোমার কাজ হবে array এর উপরে map ইউজ করে। প্রত্যেকটা উপাদানকে ৫ দিয়ে গুন্ করে গুনফল আরেকটা array হিসেবে রাখবে। পুরা কাজটা এক লাইনে হবে। 
+
+const numbers =[1,2,3,4,5,6,7,8];
+const result = numbers.map(x=>x*5);
+console.log(result); */
+
+/* // [ চ্যালেঞ্জিং। গুগলে সার্চ দিয়ে বের করো ] অনেকগুলা সংখ্যার একটা array থেকে শুধু বিজোড় সংখ্যা বের করে নিয়ে আসার জন্য filter ইউজ করো 
+
+const numbers =[1,2,3,4,5,6,7,8];
+const result = numbers.filter(x=>x%2==1);
+console.log(result); */
+
+/* //৭. একটা array এর মধ্যে অনেকগুলা অবজেক্ট আছে। সেখানে যেই অবজেক্ট এর price আছে ৫০০০ টেক্কা সেই অবজেক্টকে find দিয়ে বের করো। 
+
+const array =[
+    {name:'mobile',price:3000,ram:'2gb'},
+    {name:'mobile',price:4000,ram:'4gb'},
+    {name:'mobile',price:5000,ram:'6gb'}
+]
+const result = array.find(x=>x.price == 5000);
+console.log(result); */
+
+//৭.৫ [এক্সট্রা] জাভাস্ক্রিপ্ট এ array এর map-will return everything into array , forEach-if there is no return then we use it, filter-when there is condition and return as array, find-find the first as condition and return without array কোনটা দিয়ে কি হয়। সেটার একটা সামারি লিখে ফেলো। 
+
+/* //৮. সিম্পল একটা জাভাস্ক্রিপ্ট অবজেক্ট এর কোন একটা প্রোপার্টিকে ভেরিয়েবল হিসেবে ডিক্লেয়ার করার জন্য destructuring ইউজ করো। 
+
+const myObject = {
+    name:'alamin',
+    age :24,
+    height:6.1
+}
+const {name,age,height} = myObject;
+console.log(age); */
+
+/* //৯. [চ্যালেঞ্জিং] array এর destructuring করবে আর সেটা করার জন্য তুমি এরে এর থার্ড পজিশন এর উপাদান কে destructuring করে 'three' নামক একটা ভেরিয়েবল এ রাখবে। 
+
+const numbers =[1,2,3,4,5,6,7,8];
+const [first,second,third] = numbers;
+console.log(third); */
+
+/* //১০. তিনটা প্যারামিটার ওয়ালা একটা ফাংশন লিখবে। যেই ফাংশনের কাজ হবে তিনটা প্যারামিটার নিয়ে সেই তিনটা প্যারামিটার এর যোগ করে যোগফল রিটার্ন করবে। আর থার্ড প্যারামিটার এর একটা ডিফল্ট ভ্যালু থাকবে। সেটা হবে ৭। 
+
+function add(x,y,z=7){
+    return x+y+z;
+}
+console.log(add(1,2)); */
+
+//১১. একটা nested অবজেক্ট ডিক্লেয়ার করো (অর্থাৎ একটা অবজেক্ট এর প্রপার্টি এর মধ্যেও যে অবজেক্ট থাকতে পারে। আবার সেই অবজেক্ট এর প্রপার্টি এর মধ্যেও সে অবজেক্ট থাকতে পারে। সেই রকম একটা অবজেক্ট ডিক্লেয়ার করো। এবং যেকোন একটা প্রপার্টি এর মান একটা array হবে। জাস্ট এমন একটা অবজেক্ট )
+
+const myObject = {
+    name:'alamin',
+    age :24,
+    height:6.1,
+    student:{
+        institution :'DIU',
+        department : 'CSE',
+        others :{
+            place :'dhanmondi',
+            time:4
+        }
+    }
+} 
+
+//১২. উপরের অবজেক্ট এ ডট এর আগে যে প্রশ্নবোধক চিহ্ন দিয়ে যে অপশনাল চেইনিং করা যায়। সেটা একটু প্রাকটিস করো। 
+
+console.log(myObject?.students?.others?.place)
